b110f46d724544cfd55bb687e2daa4ebc06e9d27
ngrubii <nenadgubenovic99@gmail.com>
2022-12-17 19:30:21 +0100

    added table

--- prijavljen.php
+++ prijavljen.php
@@ -44,13 +44,215 @@ mysqli_query($db, "SET NAMES utf8");
         font-size: 16px;
         color: #9932CC;
     }
+.table>:not(:last-child)>:last-child>*{
+    background:gray;
+    color:white;
+    text-align:center;
+    border:black;
+    border-style:solid;
+    margin:auto;
+    padding:1em;
+}
+.table>thead{
+    vertical-align:middle;
+}
+.table{
+    text-align:center;
+    border:black;
+    
+}
+.table-hover>tbody>tr:hover>*{
+    background:#9932CC;
+    color:white;
+}
+.table-hover>tbody>tr{
+    border:black;
+    border-style:solid;
+    
+}
+.btn-primary{
+    background:#9932CC;
+    box-shadow:#9932CC !important;
+}
+.btn-primary:hover{
+    background:purple;
+    box-shadow:purple;
+}
+.btn-primary:active{
+color:yellow;
+}
 </style>
+
 
 <body>
     <!-- Navbar -->
 <?php
     navbar();
 ?>
+
+
+
+ <!-- TABLE -->
+
+
+<table class="table">
+  <thead class="table-dark">
+    <tr>
+      <th scope="col">Customer</th>
+      <th scope="col">Product in use</th>
+      <th scope="col">Traffic Volume</th>
+      <th scope="col">Main Competitor</th>
+      <th scope="col">Core Destinations</th>
+      <th scope="col">Destinations Looking For</th>
+      <th scope="col">Potential Destinations</th>
+      <th scope="col">Action</th>
+      <th scope="col">Next Step</th>
+      <th scope="col">Result</th>
+      <th scope="col">Date/Comment</th>
+
+    </tr>
+</thead>
+  <tbody>
+    <tr>
+      <th scope="row">IDT</th>
+      <td>Mark</td>
+      <td>otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+    </tr>
+    <tr>
+      <th scope="row">IDT</th>
+      <td>Mark</td>
+      <td>otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+    </tr>
+    <tr>
+      <th scope="row">IDT</th>
+      <td>Mark</td>
+      <td>otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+    </tr>
+    <tr>
+      <th scope="row">IDT</th>
+      <td>Mark</td>
+      <td>otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+    </tr>
+    <tr>
+      <th scope="row">TeleSign</th>
+      <td>mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+    </tr>
+    
+    <tr>
+      <th scope="row">TeleSign</th>
+      <td>mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+    </tr>
+    <tr><tr>
+      <th scope="row">TeleSign</th>
+      <td>mdo</td>
+      <td>OttoOttoOttoOttoOtto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+    </tr>
+    <tr><tr>
+      <th scope="row">TeleSign</th>
+      <td>mdo</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+    </tr>
+    <tr><tr>
+      <th scope="row">TeleSign</th>
+      <td>mdo</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+    </tr>
+    <tr>
+      <th scope="row">Twilio</th>
+      <!-- <td colspan="2">Larry the Bird</td> -->
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>
+      <td>Mark</td>    </tr>
+  
+  </tbody>
+</table>
+
+
+
+
+
+
     <!-- Navbar -->
     <script type="text/javascript" src="js/mdb.min.js"></script>
     <script type="text/javascript" src="js/loginscript.js"></script>

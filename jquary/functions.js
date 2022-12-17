653d25fee1c72e0e86cf3a08965c3fd82095aeea
beban <aleksa.aleksic99@gmail.com>
2022-12-17 21:39:35 +0100

    insertRow function added

--- jquary/functions.js
+++ jquary/functions.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-
+    if(document.getElementById("table_body")){
+        fillDataTable();
+    }
     $("#login").click(function(){
         let korIme = $("#form2Example11").val();
         let pass = $("#form2Example22").val();
@@ -21,5 +23,74 @@ $(document).ready(function(){
         })
 
     });
+
+    $("#insertRow").click(function(){
+        let ins_customer       = $("#ins_customer").html();
+        let ins_prod           = $("#ins_prod").html();
+        let ins_traff          = $("#ins_traff").html();
+        let ins_maincomp       = $("#ins_maincomp").html();
+        let ins_dest           = $("#ins_dest").html();
+        let ins_looking        = $("#ins_looking").html();
+        let ins_pot            = $("#ins_pot").html();
+        let ins_act            = $("#ins_act").html();
+        let ins_next           = $("#ins_next").html();
+        let ins_result         = $("#ins_result").html();
+        let ins_datecomm       = $("#ins_datecomm").html();
+        
+
+        console.log(ins_customer,
+                    ins_prod    ,
+                    ins_traff   ,
+                    ins_maincomp,
+                    ins_dest    ,
+                    ins_looking ,
+                    ins_pot     ,
+                    ins_act     ,
+                    ins_next    ,
+                    ins_result  ,
+                    ins_datecomm);
+                    
+
+            $.post("ajax.php?f=insertRow", 
+            {
+                ins_customer:ins_customer,
+                ins_prod    :ins_prod    ,
+                ins_traff   :ins_traff   ,
+                ins_maincomp:ins_maincomp,
+                ins_dest    :ins_dest    ,
+                ins_looking :ins_looking ,
+                ins_pot     :ins_pot     ,
+                ins_act     :ins_act     ,
+                ins_next    :ins_next    ,
+                ins_result  :ins_result  ,
+                ins_datecomm:ins_datecomm
+            }, 
+            
+            function(response){
+                fillDataTable();
+                $("#insertResp").html(response);
+            })
+            
+            
+            //RESET FIELDS
+            $("#ins_customer").html("");
+            $("#ins_prod").html("");
+            $("#ins_traff").html("");
+            $("#ins_maincomp").html("");
+            $("#ins_dest").html("");
+            $("#ins_looking").html("");
+            $("#ins_pot").html("");
+            $("#ins_act").html("");
+            $("#ins_next").html("");
+            $("#ins_result").html("");
+            $("#ins_datecomm").html("");
+
+    });
     
 })
+
+function fillDataTable(){
+    $.post("ajax.php?f=fillDataTable", function(response){
+        $("#table_body").html(response);
+    })
+}

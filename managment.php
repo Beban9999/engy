ff8cb72b3ac63754c12d1f818577df10e692f995
beban <aleksa.aleksic99@gmail.com>
2022-12-20 22:23:55 +0100

    Dynamic message display

--- managment.php
+++ managment.php
@@ -37,7 +37,7 @@ mysqli_query($db, "SET NAMES utf8");
     <link rel="stylesheet" href="css/metricacss.css" />
     <link rel="shortcut icon" href="assets/images/favicon.ico">
 
-    
+
 
 
     <script src="jquary/jquary.js"></script>
@@ -47,162 +47,107 @@ mysqli_query($db, "SET NAMES utf8");
 
 </head>
 
-    <body>
+<body>
     <?php
     navbar();
     ?>
-<br>
-                        <!-- Private Messsage MODAL -->
-                                    <div class="modal fade bs-example-modal-center" style="position:fixed; bottom:34px">
-                                                    <div class="col-md-12 col-lg-4" style="position:relative;left:34%">
-                                                        <div class="card">
-                                                            <div class="card-body">
-                                                                <h4 class="mt-0 header-title" style = "text-align:center">Private Message to <?php echo $_SESSION['username'] ?></h4>
-                                                                <form>
-                                                                    <div class="form-group row">
-                                                                        <div class="col-sm-12">
-                                                                            <input class="form-control" type="text" id="subject2" placeholder="Subject">                                                       
-                                                                        </div>                                                    
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="4" placeholder="Your message"></textarea>
-                                                                    </div>                                                
-                                                                
-                                                                    <button type="submit" class="btn btn-primary btn-block px-4" style="background:purple;color:white">Send Message</button>
-                                                                    <button type="submit" class="btn btn-primary btn-block px-4" style="background:#A52A2A;color:white">Cancel</button>
+    <br>
 
-                                                                </form>
-                                                            </div><!--end card-body-->
-                                                        </div><!--end card-->
-                                                    </div><!--end col-->
-                                                </div><!--end row-->
-                                    </div><!-- /.modal -->
-                                </div><!--end card-body-->
-                         
+    <!-- Private Messsage MODAL -->
+    <div class="modal fade bs-example-modal-center" style="position:fixed; bottom:34px">
+        <div class="col-md-12 col-lg-4" style="position:relative;left:34%">
+            <div class="card">
+                <div class="card-body">
+                    <h4 id='privateMessageHeader' class="mt-0 header-title" style="text-align:center">Private Message to USER</h4>
+                        <form>
+                            <div class="form-group">
+                                <textarea class="form-control" id="privateMessageText" rows="4" placeholder="Your message"></textarea>
+                            </div>
 
-<div class="col-md-12 col-lg-4" style="position:relative;left:34%">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h4 class="mt-0 header-title" style = "text-align:center">Global Message</h4>
-                                    <form>
-                                        <div class="form-group row">
-                                            <div class="col-sm-12">
-                                                <input class="form-control" type="text" id="subject2" placeholder="Subject">                                                       
-                                            </div>                                                    
-                                        </div>
-                                        <div class="form-group">
-                                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="4" placeholder="Your message"></textarea>
-                                        </div>                                                
-                                       
-                                        <button type="submit" class="btn btn-primary btn-block px-4" style="background:purple;color:white">Send Message</button>
-                                    </form>
-                                </div><!--end card-body-->
-                            </div><!--end card-->
-                        </div><!--end col-->
-                    </div><!--end row-->
-<br>
-                <!--TABLE -->
-                <div class="row">
-                        <div class="col-12">
-                            <div class="card">
-                                <!-- <div class="card-body"> -->
-        
-                                    <div class="table-responsive">
-                                        <table class="table table-striped mb-0">
-                                            <thead class="thead-light" style="background:purple;color:white">
-                                            <tr>
-                                                <th>Users</th>
-                                                <th>Role</th>
-                                                <th>Email</th>
-                                                <th>a</th>
-                                                <th>Message User</th>
-                                                <th>Visit Page</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            <tr>
-                                                <td><img src="../assets/images/users/user-3.jpg" alt="" class="rounded-circle thumb-sm mr-1"> Aaron Poulin</td>
-                                                <td>Agent</td>
-                                                <td>AaronPoulin@example.com</td>
-                                                <td>+21 123456789</td>
-                                                <td><button type="button" class="btn btn-success waves-effect waves-light">Message</button><button type="button" class="btn btn-danger waves-effect waves-light">Remove</button></td>
-                                                <td>                                                       
-                                                <button type="button" class="btn btn-primary waves-effect waves-light">Visit</button></td>
-                                                <i class="mdi mdi-message-text-outline"></i>
-                                            </tr>
-                                            <tr>
-                                                <td><img src="../assets/images/users/user-4.jpg" alt="" class="rounded-circle thumb-sm mr-1"> Richard Ali</td>
-                                                <td>Administrator</td>
-                                                <td>RichardAli@example.com</td>
-                                                <td>+41 123456789</td>
-                                                <td><button type="button" class="btn btn-success waves-effect waves-light">Message</button><button type="button" class="btn btn-danger waves-effect waves-light">Remove</button></td>
-                                                <td>                                                       
-                                                <button type="button" class="btn btn-primary waves-effect waves-light">Visit</button></td>
-                                            </tr>
-
-                                            <tr>
-                                                <td><img src="../assets/images/users/user-5.jpg" alt="" class="rounded-circle thumb-sm mr-1"> Juan Clark</td>
-                                                <td>Contributor</td>
-                                                <td>JuanClark@example.com</td>
-                                                <td>+65 123456789</td>
-                                                <td><button type="button" class="btn btn-success waves-effect waves-light">Message</button><button type="button" class="btn btn-danger waves-effect waves-light">Remove</button></td>
-                                                <td>                                                       
-                                                <button type="button" class="btn btn-primary waves-effect waves-light">Visit</button></td>
-                                            </tr>
-
-                                            <tr>
-                                                <td><img src="../assets/images/users/user-6.jpg" alt="" class="rounded-circle thumb-sm mr-1"> Albert Hull</td>
-                                                <td>Agent</td>
-                                                <td>AlbertHull@example.com</td>
-                                                <td>+88 123456789</td>
-                                                <td><button type="button" class="btn btn-success waves-effect waves-light">Message</button><button type="button" class="btn btn-danger waves-effect waves-light">Remove</button></td>
-                                                <td>                                                       
-                                                <button type="button" class="btn btn-primary waves-effect waves-light">Visit</button></td>
-                                            </tr>
-
-                                            <tr>
-                                                <td><img src="../assets/images/users/user-7.jpg" alt="" class="rounded-circle thumb-sm mr-1"> Crystal Darling</td>
-                                                <td>Contributor</td>
-                                                <td>CrystalDarling@example.com</td>
-                                                <td>+56 123456789</td>
-                                                <td>
-                                                    <button type="button" class="btn btn-success waves-effect waves-light" data-toggle="modal" data-animation="bounce" data-target=".bs-example-modal-center">Private Message</button>
-                                                    <button type="button" class="btn btn-danger waves-effect waves-light">Remove</button></td>
-                                                <td>                                                       
-                                                <button type="button" class="btn btn-primary waves-effect waves-light">Visit</button>
-
-                                                </td>
-
-                                            <tr>
-                                                <td><img src="../assets/images/users/user-8.jpg" alt="" class="rounded-circle thumb-sm mr-1"> Thomas Milligan</td>
-                                                <td>Administrator</td>
-                                                <td>homasMilligan@example.com</td>
-                                                <td>+35 123456789</td>
-                                                <td><button type="button" class="btn btn-success waves-effect waves-light">Message</button><button type="button" class="btn btn-danger waves-effect waves-light">Remove</button></td>
-                                                <td>                                                       
-                                                <button type="button" class="btn btn-primary waves-effect waves-light">Visit</button>
-
-                                                </a></td>
-                                            </tr>
-                                            
-                                            
-                                            </tbody>
-                                        </table><!--end /table-->
-                                    </div><!--end /tableresponsive-->
-                                <!-- </div>end card-body -->
-                            </div><!--end card-->
-                        </div> <!-- end col -->
-                    </div><!--end row-->
+                            <button type="submit" class="btn btn-primary btn-block px-4" id="sendPrivateMessage" style="background:purple;color:white">Send Message</button>
+                            <button type="submit" class="btn btn-primary btn-block px-4" data-bs-dismiss="modal" style="background:#A52A2A;color:white">Cancel</button>
+                        </form>
+                </div>
+                <!--end card-body-->
+            </div>
+            <!--end card-->
+        </div>
+        <!--end col-->
+    </div>
+    <!--end row-->
+    </div><!-- /.modal -->
+    </div>
+    <!--end card-body-->
 
 
+    <div class="col-md-12 col-lg-4" style="position:relative;left:34%">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="mt-0 header-title" style="text-align:center">Global Message</h4>
+                <div class="form-group row">
 
-    </body>
-    <script src="assets/js/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.bundle.min.js"></script>
-        <script src="assets/js/metisMenu.min.js"></script>
-        <script src="assets/js/waves.min.js"></script>
-        <script src="assets/js/jquery.slimscroll.min.js"></script>
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control" id="globalMessageText" rows="4" placeholder="Your message"></textarea>
+                </div>
 
-    <script type="text/javascript" src="js/mdb.min.js"></script>
-    <script type="text/javascript" src="js/loginscript.js"></script>
+                <button type="submit" id="sendGlobalMessage" class="btn btn-primary btn-block px-4" style="background:purple;color:white">Send Message</button>
+            </div>
+            <!--end card-body-->
+        </div>
+        <!--end card-->
+    </div>
+    <!--end col-->
+    </div>
+    <!--end row-->
+    <br>
+    <!--TABLE -->
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <!-- <div class="card-body"> -->
+
+                <div class="table-responsive">
+                    <table class="table table-striped mb-0">
+                        <thead class="thead-light" style="background:purple;color:white">
+                            <tr>
+                                <th>Users</th>
+                                <th>Role</th>
+                                <th>Email</th>
+                                <th>Message User</th>
+                                <th>Visit Page</th>
+                            </tr>
+                        </thead>
+                        <tbody id="users_table">
+                        </tbody>
+                    </table>
+                    <!--end /table-->
+                </div>
+                <!--end /tableresponsive-->
+                <!-- </div>end card-body -->
+            </div>
+            <!--end card-->
+        </div> <!-- end col -->
+    </div>
+    <!--end row-->
+    <div style="visibility:hidden" id="clickedUserId">TEST2</div>
+</body>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/metisMenu.min.js"></script>
+<script src="assets/js/waves.min.js"></script>
+<script src="assets/js/jquery.slimscroll.min.js"></script>
+
+<script type="text/javascript" src="js/mdb.min.js"></script>
+<script type="text/javascript" src="js/loginscript.js"></script>
+<script>
+    //<h4 id = 'privateMessageHeader' class="mt-0 header-title" style = "text-align:center">Private Message to USER</h4>
+
+    function saveClickedUserInfo(clickedUser, userId) {
+        document.getElementById("privateMessageHeader").innerHTML = "Private Message to " + clickedUser;
+        document.getElementById("clickedUserId").innerHTML = userId;
+
+    }
+</script>
+
 </html>

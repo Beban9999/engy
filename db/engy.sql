8da0ec709f0f89b1adb9229ecb1d015c1a6f45a1
beban <aleksa.aleksic99@gmail.com>
2022-12-20 22:26:59 +0100

    Added messages table

--- db/engy.sql
+++ db/engy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2022 at 08:51 PM
+-- Generation Time: Dec 20, 2022 at 10:26 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -53,7 +53,35 @@ INSERT INTO `data` (`customer`, `prod`, `traff`, `maincomp`, `dest`, `looking`, 
 ('', 'eeeeee', 'qewf', 'joi', 'joi', 'eeee', 'ojiwfg', 'asdasd', 'qweqwe', 'qweqwe', '123123123123', 1, 17, 0, 0),
 ('asd', 'asd', 'weqwe', '', 'qwe', '', '', '', '', '', '', 1, 18, 0, 0),
 ('', '', '', '', '', '', '', '', '', '', '', 1, 19, 0, 0),
-('', '', 'qwe', 'qwe', 'qwe', 'qweqwe', 'eee', '', '', '', '', 1, 20, 0, 0);
+('Nesa nije smrad', '', 'qwe', 'qwe', 'qwe', 'qweqwe', 'eee', '', '', '', '', 1, 20, 0, 0),
+('kita muda&nbsp;', 'sise', 'asdasd', 'asdasd', 'ewotrhi', '2', '', '', '', '', '', 1, 21, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id_message` int(10) UNSIGNED NOT NULL,
+  `message_text` text NOT NULL,
+  `user_from` int(10) UNSIGNED NOT NULL,
+  `user_for` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `message_date` datetime NOT NULL,
+  `deleted` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id_message`, `message_text`, `user_from`, `user_for`, `message_date`, `deleted`) VALUES
+(3, 'TEST TEST', 1, 0, '2022-12-20 09:24:08', 0),
+(4, 'asdasdasd', 1, 5, '2022-12-20 09:45:30', 0),
+(6, 'Test msg', 1, 5, '2022-12-20 09:51:19', 0),
+(7, 'asdasd', 1, 5, '2022-12-20 09:51:44', 0),
+(8, 'Jovo sisaj ga', 2, 5, '2022-12-20 10:11:16', 0),
+(14, 'Pisemo JOVI', 1, 5, '2022-12-20 10:22:57', 0);
 
 -- --------------------------------------------------------
 
@@ -114,6 +142,12 @@ ALTER TABLE `data`
   ADD PRIMARY KEY (`data_id`);
 
 --
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id_message`);
+
+--
 -- Indexes for table `roles`
 --
 ALTER TABLE `roles`
@@ -134,7 +168,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `data`
 --
 ALTER TABLE `data`
-  MODIFY `data_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `data_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id_message` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `roles`
@@ -146,7 +186,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables

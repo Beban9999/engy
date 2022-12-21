0767c51b0079f2398058f6b71f8314e53c7d5847
beban <aleksa.aleksic99@gmail.com>
2022-12-21 21:14:59 +0100

    Fixed modal window

--- managment.php
+++ managment.php
@@ -58,31 +58,32 @@ body{
     ?>
     <br>
 
-    <!-- Private Messsage MODAL -->
-    <div class="modal fade bs-example-modal-center" style="position:fixed; bottom:34px">
-        <div class="col-md-12 col-lg-4" style="position:relative;left:34%">
-            <div class="card">
-                <div class="card-body">
-                    <h4 id='privateMessageHeader' class="mt-0 header-title" style="text-align:center">Private Message to USER</h4>
-                        <form>
-                            <div class="form-group">
-                                <textarea class="form-control" id="privateMessageText" rows="4" placeholder="Your message"></textarea>
-                            </div>
 
-                            <button type="submit" class="btn btn-primary btn-block px-4" id="sendPrivateMessage" style="background:purple;color:white">Send Message</button>
-                            <button type="submit" class="btn btn-primary btn-block px-4" data-bs-dismiss="modal" style="background:#A52A2A;color:white">Cancel</button>
-                        </form>
-                </div>
-                <!--end card-body-->
-            </div>
-            <!--end card-->
-        </div>
-        <!--end col-->
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id='privateMessageHeader'>New </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="privateMessageText"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" id="sendPrivateMessage" class="btn btn-primary" data-dismiss="modal">Send message</button>
+      </div>
     </div>
-    <!--end row-->
-    </div><!-- /.modal -->
-    </div>
-    <!--end card-body-->
+  </div>
+</div>
 
 
     <div class="col-md-12 col-lg-4" style="position:relative;left:34%">

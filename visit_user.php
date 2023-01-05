92ea5c17baa229dadec96f982f9d00c2ccc5190d
ngrubii <nenadgubenovic99@gmail.com>
2023-01-05 17:25:49 +0100

    bugfix on visituser

--- visit_user.php
+++ visit_user.php
@@ -40,7 +40,7 @@ if (mysqli_num_rows($rez) > 0) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- MDB icon -->
-    <link rel="icon" href="logo.ico" type="image/x-icon" />
+    <link rel="icon" href="logo.png" type="image/x-icon" />
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
     <!-- Google Fonts Roboto -->
